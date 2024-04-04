b1ec08f9e4755fd5ed6df543afa60103966dd1b1
mamedovsh <shagin.mamedoff@yandex.ru>
2024-04-04 22:23:30 +0700

    Задачи семинар №4

---
+++ Seminar4/2.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+Дан массив arr (ниже).
+Необходимо внести такие изменения в массив, лежащий в const arr, чтобы
+его значения после изменений стали: 
+[1, 2, 100, 6, 7]
+*/
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// const indexToReplace = 2;
+// arr.splice(indexToReplace, 1, 100);
+// arr.splice(indexToReplace + 1, 2);
+// console.log(arr);
